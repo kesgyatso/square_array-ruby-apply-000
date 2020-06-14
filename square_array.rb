87bafe9eceaa1d6271479a78df274a7a48474b0d
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-06-14 19:21:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   new_array=[]
   
-  array.each do |integer|
-    new_array << integer ** 2
+  array.each do |int|
+    new_array << int ** 2
   end
   
   return new_array
